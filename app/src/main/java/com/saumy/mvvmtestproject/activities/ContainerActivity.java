144e72013653a3f895a562990740362a06eb7eb7
saumykukreti <saumykukreti@gmail.com>
2018-12-16 00:00:34 +0530

    Sk: Minor

--- app/src/main/java/com/saumy/mvvmtestproject/activities/ContainerActivity.java
+++ app/src/main/java/com/saumy/mvvmtestproject/activities/ContainerActivity.java
@@ -1,8 +1,12 @@
 package com.saumy.mvvmtestproject.activities;
 
 import android.databinding.DataBindingUtil;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.util.Log;
 
 import com.saumy.mvvmtestproject.fragments.dashboardfragment.DashboardFragment;
 import com.saumy.mvvmtestproject.MyApp;
@@ -19,13 +23,14 @@ import retrofit2.Response;
 
 public class ContainerActivity extends AppCompatActivity {
 
+    private static final String TAG = ContainerActivity.class.getSimpleName();
     ActivityContainerBinding mActivityContainerBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mActivityContainerBinding = DataBindingUtil.setContentView(this, R.layout.activity_container);
-        ((MyApp)getApplicationContext()).getComponent().inject(this);
+        ((MyApp) getApplicationContext()).getComponent().inject(this);
         addHomeFragment();
     }
 
@@ -35,6 +40,28 @@ public class ContainerActivity extends AppCompatActivity {
     private void addHomeFragment() {
         //Creating a dashboard fragment
         DashboardFragment dashboardFragment = DashboardFragment.newInstance();
-        this.getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,dashboardFragment).commit();
+        pushFragment(dashboardFragment, true, false);
+    }
+
+    public void pushFragment(Fragment fragment, boolean add, boolean addToBackStack) {
+        if (null != fragment && null != getSupportFragmentManager()) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            if (add)
+                transaction.add(R.id.fragment_container, fragment);
+            else
+                transaction.replace(R.id.fragment_container, fragment);
+
+            if (addToBackStack)
+                transaction.addToBackStack(fragment.getClass().getSimpleName());
+
+            transaction.commitAllowingStateLoss();
+        } else {
+            Log.e(TAG, "Passed fragment is null!");
+        }
+    }
+
+    public void openFragment() {
+
     }
 }
