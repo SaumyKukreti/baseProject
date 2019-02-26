d60b199807e2b91435ae1bcd77b61d785918ec25
saumykukreti <saumykukreti@gmail.com>
2019-02-26 15:36:08 +0530

    Added chart fragment

---
+++ app/src/main/java/com/saumy/mvvmtestproject/fragments/ChartFragment.java
@@ -0,0 +1,42 @@
+package com.saumy.mvvmtestproject.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.saumy.mvvmtestproject.R;
+
+public class ChartFragment extends Fragment {
+
+    public ChartFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static ChartFragment newInstance() {
+        ChartFragment fragment = new ChartFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_chart, container, false);
+    }
+
+}
