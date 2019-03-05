d89d6a92c9aa9db8d621fe640bc8c65c9c1e2fa3
saumykukreti <saumykukreti@gmail.com>
2019-03-05 16:11:50 +0530

    Created new activity

--- app/src/main/java/com/saumy/mvvmtestproject/activities/ImageActivity.java
+++ app/src/main/java/com/saumy/mvvmtestproject/activities/ImageActivity.java
@@ -1,21 +1,76 @@
 package com.saumy.mvvmtestproject.activities;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.saumy.mvvmtestproject.R;
+import com.saumy.mvvmtestproject.databinding.ActivityImageBinding;
 
-public class ImageActivity extends AppCompatActivity {
+public class ImageActivity extends AppCompatActivity implements ImageFragmentListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_image);
+        ActivityImageBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_image);
+        binding.setListener(this);
+    }
+
+    @Override
+    public void oneOneClicked() {
+        Toast.makeText(this, "11", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void oneTwoClicked() {
+        Toast.makeText(this, "12", Toast.LENGTH_SHORT).show();
 
     }
 
+    @Override
+    public void oneThreeClicked() {
+        Toast.makeText(this, "13", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void twoOneClicked() {
+        Toast.makeText(this, "21", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void twoTwoClicked() {
+        Toast.makeText(this, "22", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void twoThreeClicked() {
+        Toast.makeText(this, "23", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void threeOneClicked() {
+        Toast.makeText(this, "31", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void threeTwoClicked() {
+        Toast.makeText(this, "32", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void threeThreeClicked() {
+        Toast.makeText(this, "33", Toast.LENGTH_SHORT).show();
+
+    }
 }
---
+++ app/src/main/java/com/saumy/mvvmtestproject/activities/ImageFragmentListener.java
@@ -0,0 +1,27 @@
+package com.saumy.mvvmtestproject.activities;
+
+/**
+ * Created by saumy on 3/5/2019.
+ */
+
+public interface ImageFragmentListener {
+
+    void oneOneClicked();
+
+    void oneTwoClicked();
+
+    void oneThreeClicked();
+
+    void twoOneClicked();
+
+    void twoTwoClicked();
+
+    void twoThreeClicked();
+
+    void threeOneClicked();
+
+    void threeTwoClicked();
+
+    void threeThreeClicked();
+
+}
